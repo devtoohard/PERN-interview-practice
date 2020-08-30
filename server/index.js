0ad6b34e212cd70dd4f4d0755648fcff039a648f
devtoohard <e0052482@u.nus.edu>
2020-08-30 23:09:51 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//routes//
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo(description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+
+    res.json(allTodos);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo_id = $1", [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("server running!");
+});
